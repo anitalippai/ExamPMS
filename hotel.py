27098c0eead0d67bda2f6e09dff6f67800074921
anitalippai <anita.lippai93@gmail.com>
2023-11-30 16:50:54 +0000

    add methods to hotel class

--- hotel.py
+++ hotel.py
@@ -1,4 +1,13 @@
+from room import Room
+from single import Single
+from double import Double
 class Hotel:
-    def __init__(self, rooms, name):
-        self.rooms = rooms
+    def __init__(self, name):
+        self.rooms = []
         self.name = name
+
+    def add_room(self, room: Room):
+        self.rooms.append(room)
+    def upload_rooms(self):
+        self.add_room(Single(101, 2500, 'sea'))
+        self.add_room(Double(201, 5000, 'Breakfast'))
